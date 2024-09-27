510bf28d8d930e65d3ecb7797e4e25f194251997
bon9 <rnem8831@gmail.com>
2024-09-27 17:47:55 +0900

    push for 20241002 assignments

--- book-shop/routes/users.js
+++ book-shop/routes/users.js
@@ -1,38 +1,19 @@
 const express = require('express');
 const router = express.Router();
-
 router.use(express.json());
 
+const {join, login, reset_post, reset_put} = require('../controller/UserController');
+
 // 회원가입
-router.post('/join',[
-
-],(req,res) => {
-
-
-});
+router.post('/join', join);
 
 // 로그인
-router.post('/login',[
-
-],(req,res) => {
-    
-
-});
+router.post('/login', login);
 
 // 비밀번호 초기화 요청
-router.post('/reset',[
-
-],(req,res) => {
-    
-
-});
+router.post('/reset', reset_post);
 
 // 비밀번호 초기화
-router.put('/reset',[
-
-],(req,res) => {
-    
-
-});
+router.put('/reset', reset_put);
 
 module.exports = router;
